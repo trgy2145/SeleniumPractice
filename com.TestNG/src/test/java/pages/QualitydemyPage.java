fa9b08f1c0fa0602d77d113218106ee8acb290fe
trgy2145 <trgy2145@gmail.com>
2023-01-07 15:35:21 +0100

    day15 pozitif test and pages

---
+++ com.TestNG/src/test/java/pages/QualitydemyPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class QualitydemyPage {
+
+    public QualitydemyPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[text()='Log in']")
+    public WebElement firstLoginLink;
+    @FindBy(id = "login-email")
+    public WebElement emailBox;
+
+    @FindBy(id = "login-password")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//button[text()='Login']")
+    public WebElement loginButton;
+
+    @FindBy(linkText = "My courses")
+    public WebElement succesfullyLogin;
+}
---
+++ com.TestNG/src/test/java/tests/day15/C03_PozitifTest.java
@@ -0,0 +1,40 @@
+package tests.day15;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+import utilities.ReusableMethod;
+
+public class C03_PozitifTest {
+    @Test
+    public void test01(){
+
+        // https://www.qualitydemy.com/  sayfasına git
+
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+
+        // login linkine basın
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.firstLoginLink.click();
+
+        // kullanıcı email icin valid bir email girin
+        qualitydemyPage.emailBox.sendKeys("user_1104147@login.com");
+
+        // valid bir password giriniz
+        qualitydemyPage.passwordBox.sendKeys("31488081");
+
+        // login e bas ve login ol
+        qualitydemyPage.loginButton.click();
+
+        // login oldugunu test et
+        Assert.assertTrue(qualitydemyPage.succesfullyLogin.isDisplayed());
+
+        
+        ReusableMethod.wait(5);
+
+        Driver.closeDriver();
+
+
+    }
+}
